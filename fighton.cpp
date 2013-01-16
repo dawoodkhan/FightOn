e950de32da80b98ce60e1814aa0cdff6632cac00
Muhammad Dawood Khan <muhammdk@usc.edu>
2013-01-15 17:11:37 -0800

    Created the FightOn HelloWorld-like application

---
+++ fighton.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  FightOn
+//
+//  Created by Dawood Khan on 1/15/13.
+//  Copyright (c) 2013 Dawood Khan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    std::cout<<"Fight On";
+    
+}
+
